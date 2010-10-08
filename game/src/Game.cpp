7af6d0cf58bf30443965634d458894d951a7dd7f
ddelion <ddelion.ua@gmail.com>
2010-10-08 17:45:06 +0300

    Removed: All game related stuff.

--- game/src/Game.cpp
+++ game/src/Game.cpp
@@ -1,15 +1,8 @@
 #include "GameCommonIncludes.h"
 #include "GameLibraries.h"
 
-#include "EditorScreen.h"
-#include "Waypoint.h"
-#include "EditorWaypoint.h"
-
 int WINAPI WinMain(HINSTANCE /*hInstance*/, HINSTANCE /*hPrevInstance*/, LPSTR /*lpCmdLine*/, int /*nCmdShow*/)
 {
-	URegisterObject(Waypoint);
-	URegisterObject(EditorWaypoint);
-
 	rt::GameCore gameCore;
 
 	rt::OptionsManager::Get()->GetOption("title") = std::string("ReTech Engine v 0.3.0");
@@ -21,12 +14,6 @@ int WINAPI WinMain(HINSTANCE /*hInstance*/, HINSTANCE /*hPrevInstance*/, LPSTR /
 	gameCore.Initialize();
 
 	rt::RenderManager::Get()->SetRenderStatistics(true);
-
-	//rt::ScreenManager::Get()->AddScreen("main_menu", new rt::Screen("./data/worlds/main_menu_screen.world"));
-	//rt::ScreenManager::Get()->AddScreen("lobby_dwarfs", new rt::Screen("./data/worlds/lobby_dwarfs_screen.world"));
-	rt::ScreenManager::Get()->AddScreen("editor", new EditorScreen());
-
-	rt::ScreenManager::Get()->ChangeScreen("editor");
 
 	gameCore.Run();
 
